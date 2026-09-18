10d5316c2ca0735dd83904f8544372a78a4a6269
NoelJophy48 <108049576+NoelJophy48@users.noreply.github.com>
2026-09-18 09:28:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 from pathlib import Path
 
 model_path = Path(__file__).parent / "Electric_Bill.pkl"
-model = joblib.load(model_path)
+poly,model = joblib.load(model_path)
 
 st.title("AC Units")
 st.write("Enter the AC units to predict the bill.")
@@ -13,9 +13,7 @@ ac_units = st.number_input("AC_Units", min_value=0.0, step=0.5)
 
 if st.button("Predict"):
 	input_data = pd.DataFrame({"AC_Units": [ac_units]})
-	prediction = model.predict(input_data)[0]
+	input_data_poly = poly.transform(input_data)
+	prediction = model.predict(input_data_poly)[0]
 
-	if prediction == 1:
-		st.success(f"Predicted Bill: {prediction:.0f} ")
-	else:
-		st.error("Error Occurred")
+	st.success(f"Predicted Bill: {prediction:.0f} ")
