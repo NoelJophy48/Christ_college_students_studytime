7edafecb5f2dbbd01d68ec91cc8eb9a45a5daab6
NoelJophy48 <108049576+NoelJophy48@users.noreply.github.com>
2026-09-18 10:53:50 +0530

    Update model path to Electric_Bill_fan.pkl

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-model_path = Path(__file__).parent / "Electric_Bill.pkl"
+model_path = Path(__file__).parent / "Electric_Bill_fan.pkl"
 poly,model = joblib.load(model_path)
 
 st.title("AC Units")
